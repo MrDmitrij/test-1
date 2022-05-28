076c55d7c2cbb8e152f1f6e6de7e5d67041679d6
MrDmitrij <mr.dima.markevich@mail.ru>
2022-05-28 19:31:47 +0300

    init: start task

---
+++ script.js
@@ -0,0 +1,27 @@
+/* function isIsogram(str){
+  let arr = [];
+  let arr1 = str.split('');
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr.includes(arr1[i].toUpperCase())) return false;
+    arr.push(arr1[i].toUpperCase());
+  }
+  return true;
+} */
+
+function isIsogram(str){
+  return !str.match(/(.).*\1/i);
+}
+
+alert( isIsogram("Dermatoglyphics") );
+alert( isIsogram("aba") );
+alert( isIsogram("moOse") );
+alert( isIsogram("") );
+
+
+alert();
+alert();
+alert();
+alert();
+
+
+ 
